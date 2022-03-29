529b388d1ac0eabfa99898206250566299c78413
Hugo Caillard <hg.caillard@gmail.com>
2022-03-29 22:12:52 +0200

    update meta suffix

--- app/data/meta.ts
+++ app/data/meta.ts
@@ -4,7 +4,7 @@ const base = 'https://www.clearness.dev'
 
 const strip = (path: string[]) => path.map((p) => p.replace(/ |\//g, ''))
 
-const imageSuf = '19032022'
+const imageSuffix = '29032022'
 
 export const getCanonical = (path: string[] = []) =>
   path.length === 0 ? base : `${base}/${strip(path).join('/').toLowerCase()}`
@@ -36,7 +36,7 @@ export function getMeta(data: Meta) {
     'og:type': data.type,
     'og:title': data.title,
     'og:description': data.description,
-    'og:image': `${image}?${imageSuf}`,
+    'og:image': `${image}?${imageSuffix}`,
     'og:image:width': '1024',
     'og:image:height': '512',
     'og:image:alt': data.alt,
@@ -48,7 +48,7 @@ export function getMeta(data: Meta) {
     'twitter:url': url,
     'twitter:title': data.title,
     'twitter:description': data.description,
-    'twitter:image': `${image}?${imageSuf}`,
+    'twitter:image': `${image}?${imageSuffix}`,
     'twitter:image:alt': data.alt,
   }
 }
